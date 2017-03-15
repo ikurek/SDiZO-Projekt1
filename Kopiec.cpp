9fb727ba50cb4dea7e8d6f6a235b8dd1b61fd3a8
Igor Kurek <igorkurek96@gmail.com>
2017-03-15 09:59:07 +0100

    Gotowy kopiec

--- Kopiec.cpp
+++ Kopiec.cpp
@@ -25,6 +25,28 @@ void Kopiec::dodaj(int wartosc) {
         //Stwórz nową tablicę większą o jeden element
         int *nowaTablica = new int[Kopiec::rozmiar + 1];
 
+        //Przepisanie danych ze starej tablicy do nowej
+        for (int i = 0; i < Kopiec::rozmiar; i++) {
+            nowaTablica[i] = Kopiec::tablica[i];
+        }
+
+        //Dodanie wybranej wartości do kopca
+        nowaTablica[Kopiec::rozmiar] = wartosc;
+
+        //Usuń starą tablicę
+        delete[]Kopiec::tablica;
+
+        //Zastąp starą tablicę nową
+        Kopiec::tablica = nowaTablica;
+
+        //Zwiększ rozmiar kopca
+        Kopiec::rozmiar = Kopiec::rozmiar + 1;
+
+        //Popraw kolejność elementów w kopcu
+        Kopiec::poprawStrukture();
+
+
+
 
     } else {
 
@@ -34,7 +56,41 @@ void Kopiec::dodaj(int wartosc) {
 
 }
 
-void Kopiec::usun(int wartosc) {}
+void Kopiec::usun(int wartosc) {
+
+    //Iteracja po całej tablicy kopca w elu znalezienia wartości
+    for (int i = 0; i < Kopiec::rozmiar; i++) {
+
+        if (Kopiec::tablica[i] == wartosc) {
+
+            //Stwórz nową tablicę, pomniejszoną o jeden element
+            int *nowaTablica = new int[Kopiec::rozmiar - 1];
+
+            //Przepisz elementy z tablicy do pozycji na której znajduje się usuwany element
+            for (int k = 0; k < i; k++) {
+                nowaTablica[k] = Kopiec::tablica[k];
+            }
+
+            //Przepisz pozostałe elementy przesunięte o jedną pozycję
+            for (int k = i + 1; k < Kopiec::rozmiar; k++) {
+                nowaTablica[k - 1] = Kopiec::tablica[k];
+            }
+
+            //usuń starą tablicę i przypisz na jej miejsce nową
+            delete[]Kopiec::tablica;
+            Kopiec::tablica = nowaTablica;
+
+            //Zmniejsz rozmiar tablicy
+            Kopiec::rozmiar = Kopiec::rozmiar - 1;
+
+            //Popraw kolejność elementów w tablicy kopca
+            Kopiec::poprawStrukture();
+
+        }
+
+    }
+
+}
 
 bool Kopiec::sprawdzCzyIstnieje(int wartosc) {
 
@@ -52,4 +108,31 @@ bool Kopiec::sprawdzCzyIstnieje(int wartosc) {
 
 }
 
-void Kopiec::wydrukujKopiec() {}
+void Kopiec::wydrukujKopiec() {
+
+    for (int i = 0; i < Kopiec::rozmiar; i++) {
+
+        cout << "    [" << i << "] " << Kopiec::tablica[i] << endl;
+
+    }
+
+}
+
+void Kopiec::poprawStrukture() {
+
+    //Zmienna pomocnicza do pętli sortującej
+    int tymczasowa = 0;
+
+    //Pętla sortująca, układająca elementy kopca w odpowiedniej kolejności
+    //
+    for (int i = Kopiec::rozmiar; 0 < i; i--) {
+        if (Kopiec::tablica[i - 1] < Kopiec::tablica[i]) {
+            tymczasowa = Kopiec::tablica[i - 1];
+            Kopiec::tablica[i - 1] = Kopiec::tablica[i];
+            Kopiec::tablica[i] = tymczasowa;
+        }
+
+
+    }
+
+}
--- Kopiec.h
+++ Kopiec.h
@@ -18,6 +18,8 @@ public:
     bool sprawdzCzyIstnieje(int);
 
     void wydrukujKopiec();
+
+    void poprawStrukture();
 };
 
 
